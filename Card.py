3838766a0819a56c9d61702120ec8e4ac6851695
tehblasian <jeremiahdavid.wreh@gmail.com>
2019-02-12 20:14:34 -0500

    The game can be started and cards can be added onto the board

---
+++ Card.py
@@ -0,0 +1,165 @@
+from enum import Enum
+
+class Card:
+    CardColor = Enum('CARD_COLOR', 'WHITE RED')
+    CardSymbol = Enum('CARD SYMBOL', 'WHITE_DOT BLACK_DOT')
+    
+    _card_id = 1
+
+    def __init__(self, state, locationArr):
+        self._state = state
+        self._location = ''.join(locationArr)
+        
+        col = locationArr[0]
+        row = locationArr[1]
+
+        first_segment = None
+        second_segment = None
+
+        firstSegmentXLocation = self._getXLocationIndexFromLetter(col)
+        if state == 1:
+            first_segment = self.CardSegment(Card._card_id, Card.CardColor.RED, Card.CardSymbol.BLACK_DOT, firstSegmentXLocation, row)
+            secondSegmentXLocation = self._getAdjacentXLocation(self._getXLocationIndexFromLetter(col))
+            second_segment = self.CardSegment(Card._card_id, Card.CardColor.WHITE, Card.CardSymbol.WHITE_DOT, secondSegmentXLocation, row)
+
+        if state == 2:
+            first_segment = self.CardSegment(Card._card_id, Card.CardColor.RED, Card.CardSymbol.BLACK_DOT, firstSegmentXLocation, row)
+            secondSegmentYLocation = self._getYLocationIndexBelow(row)
+            second_segment = self.CardSegment(Card._card_id, Card.CardColor.WHITE, Card.CardSymbol.WHITE_DOT, firstSegmentXLocation, secondSegmentYLocation)
+
+        if state == 3:
+            first_segment = self.CardSegment(Card._card_id, Card.CardColor.WHITE, Card.CardSymbol.WHITE_DOT, firstSegmentXLocation, row)
+            secondSegmentXLocation = self._getAdjacentXLocation(self._getXLocationIndexFromLetter(col))
+            second_segment = self.CardSegment(Card._card_id, Card.CardColor.RED, Card.CardSymbol.BLACK_DOT, secondSegmentXLocation, row)
+
+        if state == 4:
+            first_segment = self.CardSegment(Card._card_id, Card.CardColor.WHITE, Card.CardSymbol.WHITE_DOT, firstSegmentXLocation, row)
+            secondSegmentYLocation = self._getYLocationIndexBelow(row)
+            second_segment = self.CardSegment(Card._card_id, Card.CardColor.RED, Card.CardSymbol.BLACK_DOT, firstSegmentXLocation, secondSegmentYLocation)
+
+        if state == 5:
+            first_segment = self.CardSegment(Card._card_id, Card.CardColor.RED, Card.CardSymbol.WHITE_DOT, firstSegmentXLocation, row)
+            secondSegmentXLocation = self._getAdjacentXLocation(self._getXLocationIndexFromLetter(col))
+            second_segment = self.CardSegment(Card._card_id, Card.CardColor.WHITE, Card.CardSymbol.BLACK_DOT, secondSegmentXLocation, row)
+
+        if state == 6:
+            first_segment = self.CardSegment(Card._card_id, Card.CardColor.RED, Card.CardSymbol.WHITE_DOT, firstSegmentXLocation, row)
+            secondSegmentYLocation = self._getYLocationIndexBelow(row)
+            second_segment = self.CardSegment(Card._card_id, Card.CardColor.WHITE, Card.CardSymbol.BLACK_DOT, firstSegmentXLocation, secondSegmentYLocation)
+
+        if state == 7:
+            first_segment = self.CardSegment(Card._card_id, Card.CardColor.WHITE, Card.CardSymbol.BLACK_DOT, firstSegmentXLocation, row)
+            secondSegmentXLocation = self._getAdjacentXLocation(self._getXLocationIndexFromLetter(col))
+            second_segment = self.CardSegment(Card._card_id, Card.CardColor.RED, Card.CardSymbol.WHITE_DOT, secondSegmentXLocation, row)
+
+        if state == 8:
+            first_segment = self.CardSegment(Card._card_id, Card.CardColor.WHITE, Card.CardSymbol.BLACK_DOT, firstSegmentXLocation, row)
+            secondSegmentYLocation = self._getYLocationIndexBelow(row)
+            second_segment = self.CardSegment(Card._card_id, Card.CardColor.RED, Card.CardSymbol.WHITE_DOT, firstSegmentXLocation, secondSegmentYLocation)
+
+        self._segments = [first_segment, second_segment]
+        Card._card_id += 1
+
+    def getLocation(self):
+        return self._location
+
+    def getState(self):
+        return self._state
+
+    def getSegments(self):
+        return self._segments
+
+    def setLocation(self, location):
+        self._location = location
+    
+    def setState(self, state):
+        self._state = state
+
+    def _getXLocationIndexFromLetter(self, rowLetter):
+        indices = {
+            'A': 1,
+            'B': 2,
+            'C': 3,
+            'D': 4,
+            'E': 5,
+            'F': 6,
+            'G': 7,
+            'H': 8,
+        }
+
+        return indices[rowLetter]
+
+    def _getAdjacentXLocation(self, rowIndex):
+        return int(rowIndex) + 1
+    
+    def _getYLocationIndexBelow(self, colIndex):
+        return int(colIndex) - 1
+
+    def __str__(self):
+        return '''
+        State: {}
+        Location: {}
+        ID: {}
+        Segments: \n{}\n{}
+        '''.format(self._state, self._location, self._card_id, str(self._segments[0]), str(self._segments[1]))
+
+    # def _getAdjacentLocation(self, state, col, row):
+
+    #     right = {
+    #         'A': 'B',
+    #         'B': 'C',
+    #         'C': 'D',
+    #         'D': 'E',
+    #         'E': 'F',
+    #         'F': 'G',
+    #         'H': None,
+    #     }
+
+    #     if state in [1, 3, 5, 7]:
+    #         return '{}{}'.format(right[col.upper()], row)
+    #     elif state in [2, 4, 6, 8]:
+    #         return '{}{}'.format(row, row - 1)
+    #     else:
+    #         raise ValueError()
+        
+    class CardSegment:
+        def __init__(self, parent, color, symbol, locationX, locationY):
+            self._parent = parent
+            self._color = color
+            self._symbol = symbol
+            self._locationX = locationX
+            self._locationY = 12 - int(locationY)
+
+        def getParent(self):
+            return self._parent
+
+        def getColor(self):
+            if self._color == Card.CardColor.RED:
+                return 'RED'
+                
+            return 'WHITE'
+
+        def getSymbol(self):
+            if self._symbol == Card.CardSymbol.WHITE_DOT:
+                return 'WDOT'
+            
+            return 'BDOT'
+
+        def getLocationX(self):
+            return self._locationX
+
+        def getLocationY(self):
+            return self._locationY
+
+        def setLocation(self, location):
+            self._location = location
+
+        def __str__(self):
+            return '''
+            Parent ID: {}
+            Color: {}
+            Symbol: {}
+            X Location: {}
+            Y Location: {}
+            '''.format(self._parent, self._color, self._symbol, self._locationX, self._locationY)
+        
---
+++ DoubleCardGame.py
@@ -0,0 +1,51 @@
+from board import Board
+from Player import Player
+
+class DoubleCardGame:
+    def __init__(self, board, players):
+        self._board = board
+        self._players = players
+
+    def playGame(self):
+        while True:
+            self._playRound()
+
+    def getPlayers(self):
+        return self._players
+
+    def _playRound(self):
+        for player in self._players:
+            player.takeTurn()
+            self._board.printBoard()
+
+if __name__ == '__main__':
+    print('Let\'s play DoubleCardGame!\n')
+        
+    mode = input('Select the game mode:\n\t1 - Human vs. Human\n\t2 - Human vs. AI\n')
+    if int(mode) == 2:
+        print('AI NOT IMPLEMEMTED')
+        exit(0)
+        
+    player1_name = input('\nEnter player 1\'s name: ')
+    player1_marker = input('\nSelect player 1\'s marker:\n1 - Dots \n2 - Color\n')
+    player2_marker = None
+    if int(player1_marker) == 1:
+        player1_marker = Player.Marker.DOTS
+        player2_marker = Player.Marker.COLOR
+    elif int(player1_marker) == 2:
+        player1_marker = Player.Marker.COLOR
+        player2_marker = Player.Marker.DOTS
+    
+    player2_name = input('\nEnter player 2\'s name: \n')
+
+    board = Board()
+    player1 = Player(player1_name, player1_marker, board)
+    player2 = Player(player2_name, player2_marker, board)
+    
+    game = DoubleCardGame(board, [player1, player2])
+
+    game.playGame()
+        
+        
+
+
