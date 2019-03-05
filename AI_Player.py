f7a4ff310fb5ab82a458905a2dc8061b104f0107
tehblasian <jeremiahdavid.wreh@gmail.com>
2019-03-05 16:43:39 -0500

    Refactored takeTurn in ai player class in preparation for recycle ai

--- AI_Player.py
+++ AI_Player.py
@@ -10,12 +10,14 @@ class AI(Player):
         self._cache = dict()
 
     def takeTurn(self):
-        #self.regular_minimax()
-        self.tourneyAI()
+        if len(self._cards) < self._maxCardsAllowed:
+            self._regularMove()
+        else:
+            self._recycleMove()
 
-    def regular_minimax(self):
+    def _regularMove(self):
         start_time = time.time()
-        state, position, score = self._board.regular_minimax(self._board, 2, True, self._marker)
+        state, position, score = self._regular_minimax()
 
         col, row = position
         card = Card(state, [str(self._board._getColumnLetterFromIndex(col)), str(row)])
@@ -26,18 +28,19 @@ class AI(Player):
 
         return self._board.addCard(card)
 
+    def _recycleMove(self):
+        pass
+
+    def _regular_minimax(self):
+        return self._board.regular_minimax(self._board, 2, True, self._marker)
+
     def tourneyAI(self):
-        start_time = time.time()
+        state = None
+        position = None
+        score = None
 
         for i in range(1, 3):
             state, position, score = self._board.minimax(self._board, i, -math.inf, math.inf, True, self._marker, self._cache)
 
-        col, row = position
-        card = Card(state, [str(self._board._getColumnLetterFromIndex(col)), str(row)])
-        self._cards.append(card)
-
-        letter = self._board._getColumnLetterFromIndex(col)
-        print('{} took {}s to play {} {} {}. Move score was {}'.format(self._name, (time.time() - start_time), state, letter, row, score))
-
-        return self._board.addCard(card)
+        return state, position, score
         
