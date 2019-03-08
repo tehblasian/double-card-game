2062eaa806a141c4432c1cfc3f0b25ff03834a35
tehblasian <jeremiahdavid.wreh@gmail.com>
2019-03-07 21:26:04 -0500

    Recycle ai

--- AI_Player.py
+++ AI_Player.py
@@ -5,8 +5,9 @@ from Card import Card
 
 class AI(Player):
     
-    def __init__(self, name, marker, board, maxCardsAllowed):
+    def __init__(self, name, marker, board, maxCardsAllowed, _type='REGULAR'):
         Player.__init__(self, name, marker, board, maxCardsAllowed)
+        self._type = _type
         self._cache = dict()
 
     def takeTurn(self):
@@ -17,7 +18,10 @@ class AI(Player):
 
     def _regularMove(self):
         start_time = time.time()
-        state, position, score = self._regular_minimax()
+        if self._type == 'REGULAR':
+            state, position, score = self._regular_minimax()
+        elif self._type == 'TOURNEY':
+            state, position, score = self.tourneyAI()
 
         col, row = position
         card = Card(state, [str(self._board._getColumnLetterFromIndex(col)), str(row)])
@@ -29,7 +33,25 @@ class AI(Player):
         return self._board.addCard(card)
 
     def _recycleMove(self):
-        pass
+        start_time = time.time()
+        
+        state = None
+        position = None
+        score = None
+        if self._type == 'REGULAR':
+            state, position, score = self._board.regular_recycle_minimax(self._board, 2, self._marker)
+        elif self._type == 'TOURNEY':
+            for i in range(1,2):
+                state, position, score = self._board.recycle_minimax(self._board, i, self._marker, self._cache)
+
+        col, row = position
+        card = Card(state, [str(self._board._getColumnLetterFromIndex(col)), str(row)])
+
+        letter = self._board._getColumnLetterFromIndex(col)
+        print('{} took {}s to recycle and play {} {} {}. Move score was {}'.format(self._name, (time.time() - start_time), state, letter, row, score))
+
+        return self._board.addCard(card, recycled=True)
+
 
     def _regular_minimax(self):
         return self._board.regular_minimax(self._board, 2, True, self._marker)
